80a83bfac41272495cafd36f210f0db8ebc31e47
callahanhirrel <callahan.hirrel@gmail.com>
2019-02-05 08:30:11 -0600

    last commit

---
+++ Game-Dev-Lab-1/Assets/Scripts/HUDFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDFollow : MonoBehaviour
+{
+    public float turnSpeed = 4.0f;
+    public Transform player;
+    private Vector3 offset;
+    private Vector3 sphe;
+    private Vector3 cam;
+    public float yOffset = 0.1f;
+    public float zOffset = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphe = new Vector3(player.position.x, player.position.y, player.position.z);
+        cam = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        offset = cam - sphe;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ Game-Dev-Lab-1/Assets/Scripts/controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+
+    public float speed;
+    public Transform relativeTransform;
+    public float time;
+    public int coinsLeft;
+    public TextMesh timeText;
+    public TextMesh coinText;
+    private bool clockRun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10;
+        time = 0f;
+        coinsLeft = 10;
+        timeText.text = "Time: 0";
+        coinText.text = "Coins Left: 10";
+        clockRun = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clockRun)
+        {
+            time += Time.deltaTime;
+            timeText.text = $"Time: {time}";
+        }
+
+        Vector3 moveDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow)) moveDirection += relativeTransform.forward;
+        if (Input.GetKey(KeyCode.DownArrow)) moveDirection += -relativeTransform.forward;
+        if (Input.GetKey(KeyCode.RightArrow)) moveDirection += relativeTransform.right;
+        if (Input.GetKey(KeyCode.LeftArrow)) moveDirection += -relativeTransform.right;
+
+        moveDirection.y = 0f;
+
+        transform.position += moveDirection.normalized * speed * Time.deltaTime;
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f,
+        //                    Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            coinsLeft -= 1;
+            coinText.text = $"Coins Left: {coinsLeft}";
+            if (coinsLeft == 0)
+            {
+                clockRun = false;
+            }
+        }
+    }
+}
